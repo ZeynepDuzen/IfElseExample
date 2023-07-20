f4717322ad82cd6bb0299bdd5523dd337d1d534f
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-20 10:22:51 +0300

    Proje dosyası ekle.

---
+++ IfElseExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi;
+            //Console.WriteLine("Lütfen sayiyi giriniz : ");
+            //sayi = Convert.ToInt16(Console.ReadLine());
+
+            //if (sayi==30)
+            //{
+            //    Console.WriteLine("Sayi dogru girildi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sayi yanlis girildi");
+            //}
+
+            int s1, s2, ort;
+            Console.WriteLine("Sayi 1 : ");
+            s1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Sayi 2 : ");
+            s2 = Convert.ToInt16(Console.ReadLine());
+            ort = (s1 + s2) / 2;
+            Console.WriteLine("Ortalamanız : " + ort);
+
+            if (ort>=50)
+            {
+                Console.WriteLine("Gecti");
+            }
+            else
+            {
+                Console.WriteLine("Kaldı");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ IfElseExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("IfElseExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IfElseExample")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("50a7d52e-c500-4d45-ae76-197becfea7f5")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
